6403ba1ca24103e2288ce5a5685f8963462ec673
Anas Bouzoubaa <anas.bouzoubaa@gmail.com>
2015-10-14 16:45:41 -0400

    Added profile image selection; more profile completion logic; request access to calendars & contacts

--- MeetMe/MeetMe/ProfileCreationViewController.m
+++ MeetMe/MeetMe/ProfileCreationViewController.m
@@ -8,12 +8,15 @@
 
 #import "ProfileCreationViewController.h"
 #import <EventKit/EventKit.h>
+#import <Contacts/Contacts.h>
+#import <PKImagePicker/PKImagePickerViewController.h>
 
 const int BUTTON_CORNER_RADIUS = 4.f;
 
-@interface ProfileCreationViewController () <UITextFieldDelegate>
+@interface ProfileCreationViewController () <UITextFieldDelegate, PKImagePickerViewControllerDelegate>
 
 @property (strong, nonatomic) IBOutlet UITextField *nameTextField;
+@property (strong, nonatomic) UIImage *selectedImage;
 @property (strong, nonatomic) IBOutlet UIImageView *profilePicture;
 @property (assign, nonatomic) IBOutlet UIImageView *backgroundImageView;
 @property (strong, nonatomic) IBOutlet UIButton *calendarAccessButton;
@@ -22,6 +25,10 @@ const int BUTTON_CORNER_RADIUS = 4.f;
 @end
 
 @implementation ProfileCreationViewController
+
+//------------------------------------------------------------------------------------------
+#pragma mark - View Lifecycle -
+//------------------------------------------------------------------------------------------
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -66,7 +73,7 @@ const int BUTTON_CORNER_RADIUS = 4.f;
 - (void)viewDidAppear:(BOOL)animated
 {
     // Show keyboard
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [self.nameTextField becomeFirstResponder];
     });
 }
@@ -74,6 +81,18 @@ const int BUTTON_CORNER_RADIUS = 4.f;
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
+
+- (IBAction)tappedFinish:(id)sender
+{
+    if (self.nameTextField.text.length == 0) {
+        [self flashTextFieldBackground];
+        [self.nameTextField becomeFirstResponder];
+    }
+}
+
+//------------------------------------------------------------------------------------------
+#pragma mark - Handling the textfield -
+//------------------------------------------------------------------------------------------
 
 - (void)touchedTextField
 {
@@ -89,30 +108,66 @@ const int BUTTON_CORNER_RADIUS = 4.f;
     }
 }
 
-- (void)changeImage
-{
-    NSLog(@"Change profile picture");
-}
-
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
     [textField resignFirstResponder];
     return YES;
 }
 
+- (void)flashTextFieldBackground
+{
+    UIColor *color = self.nameTextField.backgroundColor;
+    NSTimeInterval duration = 0.3;
+    
+    [UIView animateWithDuration:duration animations:^{
+        self.nameTextField.backgroundColor = [UIColor colorWithRed:0.710 green:0.267 blue:0.267 alpha:1.000];
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:duration animations:^{
+            self.nameTextField.backgroundColor = color;
+        }];
+    }];
+}
+
+//------------------------------------------------------------------------------------------
+#pragma mark - Profile Image -
+//------------------------------------------------------------------------------------------
+
+- (void)changeImage
+{
+    PKImagePickerViewController *imagePicker = [[PKImagePickerViewController alloc]init];
+    imagePicker.delegate = self;
+    [self presentViewController:imagePicker animated:YES completion:nil];
+}
+
+- (void)imageSelected:(UIImage *)img
+{
+    [self.profilePicture setImage:img];
+    self.selectedImage = img;
+}
+
+- (void)imageSelectionCancelled
+{
+    // Nothing to do
+}
+
+//------------------------------------------------------------------------------------------
+#pragma mark - Access Calendar & Contacts -
+//------------------------------------------------------------------------------------------
+
 -(IBAction)requestAccessToEvents
 {
-//    EKEventStore *store = [[EKEventStore alloc] init];
-//    [store requestAccessToEntityType:EKEntityTypeEvent completion:^(BOOL granted, NSError *error) {
-//        // handle access here
-//    }];
-    
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"\"Rendez-vous\" Would Like to Access Your Calendar" message:@"" preferredStyle:UIAlertControllerStyleAlert];
-    UIAlertAction *ok = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
-    UIAlertAction *no = [UIAlertAction actionWithTitle:@"Don\'t Allow" style:UIAlertActionStyleCancel handler:nil];
-    [alert addAction:ok];
-    [alert addAction:no];
-    [self presentViewController:alert animated:YES completion:nil];
+    EKEventStore *store = [[EKEventStore alloc] init];
+    [store requestAccessToEntityType:EKEntityTypeEvent completion:^(BOOL granted, NSError *error) {
+        // handle access here
+    }];
+}
+
+-(IBAction)requestAccessToContacts:(id)sender
+{
+    CNContactStore *store = [[CNContactStore alloc] init];
+    [store requestAccessForEntityType:CNEntityTypeContacts completionHandler:^(BOOL granted, NSError * _Nullable error) {
+        // handle access here
+    }];
 }
 
 @end
