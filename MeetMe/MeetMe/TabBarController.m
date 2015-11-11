b3350130fe71a2c94d39ce89f059cdd89e4a5b1b
Anas Bouzoubaa <anas.bouzoubaa@gmail.com>
2015-11-11 17:10:43 -0500

    Added create button in tab bar

--- MeetMe/MeetMe/TabBarController.m
+++ MeetMe/MeetMe/TabBarController.m
@@ -16,11 +16,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    UIButton* button = [UIButton buttonWithType:UIButtonTypeCustom];
+    UIImage *buttonImage = [UIImage imageNamed:@"Create-selected"];
+    button.frame = CGRectMake(0.0, 0.0, buttonImage.size.width, buttonImage.size.height);
+    [button setBackgroundImage:buttonImage forState:UIControlStateNormal];
+    CGFloat heightDifference = buttonImage.size.height - self.tabBar.frame.size.height;
+    if (heightDifference < 0) {
+        button.center = self.tabBar.center;
+    } else {
+        CGPoint center = self.tabBar.center;
+        center.y -= heightDifference / 3.f;
+        button.center = center;
+    }
+    [self.view addSubview:button];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
