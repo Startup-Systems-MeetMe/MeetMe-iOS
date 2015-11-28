dd1faa95444b5b321b992e1c69c52f65ec408770
Anas Bouzoubaa <anas.bouzoubaa@gmail.com>
2015-11-28 18:36:21 -0500

    Added push notifications (+ moved some files around)

---
+++ MeetMe/MeetMe/Model/Friends.m
@@ -0,0 +1,13 @@
+//
+//  Friends.m
+//  MeetMe
+//
+//  Created by Anas Bouzoubaa on 28/11/15.
+//  Copyright © 2015 Anas Bouzoubaa. All rights reserved.
+//
+
+#import "Friends.h"
+
+@implementation Friends
+
+@end
